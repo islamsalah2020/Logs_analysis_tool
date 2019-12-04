95f2fd5822432a0514ae03e7bcf79e15dc760364
islamsalah2020 <islamsalah830@gmail.com>
2019-12-04 02:00:56 +0200

    adding projectt files

---
+++ README.md
@@ -0,0 +1,41 @@
+# Logs Analysis Project
+## Project Overview
+This is an internal reporting tool that produces some Info out in the plain text in the Terminal.
+The tool produces answers to the following three questions based on the data in the database:
+
+1- What are the most popular three articles of all time?
+2- Who are the most popular articles authors of all time?
+3- On which days did more than 1% of requests lead to errors?
+
+This tool is a **Python** program that uses the **psycopg2** module to connect to the database.
+
+## Getting started
+you need to install:
+-VirtualBox
+-Vagrant
+
+## Running the Project:
+### Navigate to the directory that containing the **vagrantfile**
+-Vagrantfile containing:
+1-Configuration of the VM.
+2-Install Python
+3-Install Postgres Database.
+3-Creating Database **news**.
+
+### Run the following command to start the VM ssh into the vagrant box:
+`vagrant up`
+
+### Run the following command to ssh into the VM:
+`vagrant ssh`
+
+### After login to the VM, Extract  newsdata.zip
+
+### Run the following command to create tables and insert data into news database:
+`psql -d news -f newsdata.sql`
+
+### Connect to the database and create the views where in **Views.txt**
+
+### Run Python Program **loganalysis.py** and log analysis will be printed
+python loganalysis.py
+
+
---
+++ loganalysis.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import psycopg2
+
+#connecting to Database news to can retrieve data
+def connect(db_name="news"):
+
+    try:
+        db = psycopg2.connect(dbname=db_name)
+        return db
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+# #List the top 3 favorite articles.
+def top_articles():
+    db = connect()
+    cur = db.cursor()
+    query1 = """SELECT title, count(*) as num 
+                FROM log , articles  
+                WHERE status='200 OK' and replace(path, '/article/', '') = slug 
+                GROUP BY title
+                order by num desc
+                LIMIT 3"""
+    cur.execute(query1)
+    res = cur.fetchall()
+    print('\nthe most popular three articles of all time:')
+    j=1
+    for i in res:
+        print '{} {}, {} views'.format(j,i[0],i[1]) # title, no. of views
+        j=j+1
+    cur.close() 
+    db.close()
+
+#List the top 3 popular authors.
+def top_authors():
+    db = connect()
+    cur = db.cursor()
+    query2 = """SELECT name, (articles_view.num) as num 
+            FROM  authors, articles_view
+            WHERE authors.id = articles_view.author 
+            GROUP BY name, num
+            order by num desc
+            LIMIT 3"""
+
+    cur.execute(query2)
+    res = cur.fetchall()
+    print('\nthe most popular article authors of all time:')
+    j=1
+    for i in res:
+        print '{} {}, {} views'.format(j,i[0],i[1])
+        j=j+1
+    cur.close()
+    db.close()
+
+def error_percentage():
+    
+    db = connect()
+    cur = db.cursor()
+    
+    query3 = """select * from error_rate where ratio > 1"""
+    cur.execute(query3)
+    res = cur.fetchall()
+    
+    print ('\nThe days with error rate more than 1% :')
+    
+    for i in res:
+        print('{}, {}% errors'.format(i[0], i[1]))
+    
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    top_articles()
+    top_authors()
+    error_percentage()
+
